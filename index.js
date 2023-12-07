aaa87bf58d3c24874b29ca86a3d6b9c19ee8250d
Estlib <75094683+Estlib@users.noreply.github.com>
2023-12-07 15:09:24 +0200

    As an app developer I want to be able to delete a game [Delivers #186547973]

--- index.js
+++ index.js
@@ -81,6 +81,16 @@ app.post('/games', (req,res) => {
         .send(game)
 })
 
+app.delete('/games/:id', (req, res) => {
+    if(typeof games[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({error: "Game not found"})
+    }
+
+    games.splice(req.params.id - 1, 1)
+
+    res.status(204).send({error: "No Content"})
+})
+
 app.listen(port, () => {
     console.log(`Api up at: Http://localhost:${port}`)})
 
