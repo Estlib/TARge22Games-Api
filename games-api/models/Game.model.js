cb59331d68331bbe7feb31bda6f020150876bb28
Estlib <75094683+Estlib@users.noreply.github.com>
2023-12-15 17:50:30 +0200

    THIS SHIT WORKS RN. andmebaas saadakse kätte, tabel on olemas, games tabel ainult

--- games-api/models/Game.model.js
+++ games-api/models/Game.model.js
@@ -35,7 +35,7 @@ module.exports = (sequelize,Sequelize) => {
         },
         microtransactions: {            
             type: Sequelize.BOOLEAN,
-            allowNull: true,
+            allowNull: false,
             defaultvalue: false
         }
     })
