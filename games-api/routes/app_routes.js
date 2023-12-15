dfe68c5184e84fa4b97f6027c6be5d80734ca0bd
Estlib <75094683+Estlib@users.noreply.github.com>
2023-12-15 18:08:33 +0200

    added get method for database

--- games-api/routes/app_routes.js
+++ games-api/routes/app_routes.js
@@ -1,5 +1,9 @@
 const gamesController = require("../controllers/GamesController");
 
 module.exports = (app) => {
-    app.route("/games").get(gamesController.getAll)
+    app.route("/games")
+        .get(gamesController.getAll)
+
+    app.route("/games/:id")
+        .get(gamesController.getById)
 }
